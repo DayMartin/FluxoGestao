63bb1c5f42d493b7b3667cc3f17f2ee4ca7cb445
DayMartin <negociosdinah@gmail.com>
2023-09-01 18:09:26 -0300

    createLightTheme

--- front/conecta-os/src/App.tsx
+++ front/conecta-os/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import './App.css';
 import AppRoutes from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import LightTheme from "./shared/themes/Light";
 
 function App() {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
     <div className="App">
       <AppRoutes/>
     </div>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ front/conecta-os/src/shared/themes/Light.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+import { cyan, blue } from "@mui/material/colors";
+
+function createLightTheme() {
+  return createTheme({
+    palette: {
+      primary: {
+        main: blue[900],
+        dark: blue[800],
+        light: blue[500],
+        contrastText: '#ffffff'
+      },
+      secondary: {
+        main: cyan[500],
+        dark: cyan[400],
+        light: cyan[300],
+        contrastText: '#ffffff',
+      },
+      background: {
+        default: '#f7f6f3',
+        paper: '#ffffff',
+      }
+    }
+  });
+}
+
+export default createLightTheme;
