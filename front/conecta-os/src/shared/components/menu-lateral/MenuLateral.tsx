0a74736b8a49388fc2567c1d312327d421ae4db8
DayMartin <negociosdinah@gmail.com>
2023-09-04 10:23:59 -0300

    Menu lateral front

---
+++ front/conecta-os/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,44 @@
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import PropTypes from 'prop-types'; // Importe o PropTypes
+
+interface IMenuLateralProps {
+  children: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant='permanent'>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar 
+              sx={{ height: theme.spacing(12), width: theme.spacing(12)}}
+              src='/static/images/avatar/1.jpg'
+            ></Avatar>
+          </Box>   
+          <Divider/>
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>  
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>    
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>  
+    </>    
+  );
+};
+
+// Validação das props usando PropTypes
+MenuLateral.propTypes = {
+  children: PropTypes.node.isRequired,
+};
