e7686e68c1074dbf409fbc65649042ac0b40464a
DayMartin <negociosdinah@gmail.com>
2023-09-11 19:46:12 -0300

    Correção MenuLateral Icon

--- front/src/shared/components/menu-lateral/MenuLateral.tsx
+++ front/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,12 +1,8 @@
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
-import PropTypes from 'prop-types'; // Importe o PropTypes
-import { useAppThemeContext, useDrawerContext } from '../../contexts';
-import React from 'react';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
+import { Box } from '@mui/system';
 
-interface IMenuLateralProps {
-  children: React.ReactNode;
-}
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 interface IListItemLinkProps {
   to: string;
@@ -16,78 +12,80 @@ interface IListItemLinkProps {
 }
 const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
   const navigate = useNavigate();
+
   const resolvedPath = useResolvedPath(to);
-  const match = useMatch({path: resolvedPath.pathname, end: false});
-  
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+
   const handleClick = () => {
     navigate(to);
     onClick?.();
   };
 
   return (
-    <ListItemButton selected={!!match} onClick={handleClick} >
+    <ListItemButton selected={!!match} onClick={handleClick}>
       <ListItemIcon>
-        <Icon>icon</Icon>
-      </ListItemIcon>  
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
       <ListItemText primary={label} />
     </ListItemButton>
   );
 };
+
+interface IMenuLateralProps {
+  children: React.ReactNode;
+}
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
-  const smDown = useMediaQuery(theme.breakpoints.down('sm'));  
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const {isDrawerOpen, toggleDrawerOpen, drawerOptions} = useDrawerContext();
-
-const { toggleTheme } = useAppThemeContext();
+  const { isDrawerOpen, drawerOptions, toggleDrawerOpen } = useDrawerContext();
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <>
       <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-            <Avatar 
-              sx={{ height: theme.spacing(12), width: theme.spacing(12)}}
-              src='/static/images/avatar/1.jpg'
-            ></Avatar>
-          </Box>   
-          <Divider/>
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="#"
+            />
+          </Box>
+
+          <Divider />
+
           <Box flex={1}>
             <List component="nav">
-              {drawerOptions.map(drawerOptions => (
+              {drawerOptions.map(drawerOption => (
                 <ListItemLink
-                  to={drawerOptions.path}
-                  key={drawerOptions.path}
-                  icon={drawerOptions.icon}
-                  label={drawerOptions.label}
+                  to={drawerOption.path}
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  label={drawerOption.label}
                   onClick={smDown ? toggleDrawerOpen : undefined}
-              />
+                />
               ))}
-
             </List>
           </Box>
 
           <Box>
             <List component="nav">
-              <ListItemButton onClick={toggleTheme} >
+              <ListItemButton onClick={toggleTheme}>
                 <ListItemIcon>
                   <Icon>dark_mode</Icon>
-                </ListItemIcon>  
-              <ListItemText primary="Alterar tema" />
-          </ListItemButton>
-
+                </ListItemIcon>
+                <ListItemText primary="Alternar tema" />
+              </ListItemButton>
             </List>
           </Box>
-        </Box>    
+        </Box>
       </Drawer>
+
       <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
-      </Box>  
-    </>    
+      </Box>
+    </>
   );
-};
-
-// Validação das props usando PropTypes
-MenuLateral.propTypes = {
-  children: PropTypes.node.isRequired,
 };
