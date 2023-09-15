303447e7a81266d91042ce970505112b333e6f5e
DayMartin <negociosdinah@gmail.com>
2023-09-15 04:00:34 -0300

    hook debounce para evitar buscas desnecessárias

--- front/src/pages/Ordem/OrdemListagem.tsx
+++ front/src/pages/Ordem/OrdemListagem.tsx
@@ -3,15 +3,19 @@ import { BarraDeFerramentas, OrdemForms } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { useEffect, useMemo } from 'react';
 import { OrdemService } from '../../shared/services/api';
+import { useDebounce } from '../../shared/hooks';
 
 export const OrdemListagem: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce(2000);
 
     const busca = useMemo(() => {
       return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
+
+      debounce(() => {
 
       OrdemService.getAll(1, busca)
       .then((result) => {
@@ -21,6 +25,7 @@ export const OrdemListagem: React.FC = () => {
           console.log(result);
         }
       });
+    });
     }, [busca]);
 
     return (
