ec9ae3d9ed8d60ac5d124f87649ead4375d8e1db
DayMartin <negociosdinah@gmail.com>
2023-09-15 02:17:46 -0300

    setSearchParams, busca fixa na URL

--- front/src/pages/Ordem/OrdemListagem.tsx
+++ front/src/pages/Ordem/OrdemListagem.tsx
@@ -1,12 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
 import { BarraDeFerramentas, OrdemForms } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useMemo } from 'react';
 
-export const OrdemListagem = () => {
+export const OrdemListagem: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
     return (
         <LayoutBaseDePagina 
         titulo='Listagem de Ordem de serviços' 
         > 
-        <BarraDeFerramentas mostrarInputBusca/>
+        <BarraDeFerramentas 
+        mostrarInputBusca
+        textoBotaoNovo='Nova'
+        textoDabusca={busca}
+        aoMudarTextoDeBusca={texto => setSearchParams({busca: texto}, { replace: true})}
+        
+        />
         </LayoutBaseDePagina>
       );
 
