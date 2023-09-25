5898451e825920f8d0c139d9ef4db4338b486f15
DayMartin <negociosdinah@gmail.com>
2023-09-25 20:09:07 -0300

    Filter buscando corretamente o ordemId

--- back/controllers/ordemController.js
+++ back/controllers/ordemController.js
@@ -32,27 +32,95 @@ const ordemController = {
             console.log(error);
         }
     },
+    
     getAll: async (req, res) => {
-        try {
-            const ordens = await OrdemModel.find();
-            const totalCount = await OrdemModel.countDocuments();
-            
-            res.setHeader('x-total-count', totalCount);
-            res.json(ordens);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: "Erro ao buscar ordens de serviço" });
+      try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+    
+        const filter = req.query.filter || '';
+    
+        // Ajuste para buscar o ordemId usando o filtro
+        const query = OrdemModel.find();
+    
+        if (filter) {
+          // Use o filtro para buscar o ordemId
+          query.where({ ordemId: parseInt(filter) });
         }
+    
+        const ordem = await query
+          .skip(skip)
+          .limit(limit);
+    
+        const totalCount = await OrdemModel.countDocuments();
+    
+        const results = {
+          totalOrdem: totalCount,
+          pageCount: Math.ceil(totalCount / limit),
+        };
+    
+        if (page < results.pageCount) {
+          results.next = {
+            page: page + 1,
+          };
+        }
+    
+        if (page > 1) {
+          results.prev = {
+            page: page - 1,
+          };
+        }
+    
+        res.setHeader('x-total-count', totalCount);
+    
+        const formattedResponse = {
+          ordem: ordem.map((item) => ({
+            _id: item._id,
+            ordemId: item.ordemId,
+            solicitante: item.solicitante,
+            setor: item.setor,
+            sala: item.sala,
+            forno: item.forno,
+            cabeceira: item.cabeceira,
+            status: item.status,
+            services: item.services,
+            comments: item.comments,
+            urgencia: item.urgencia,
+          })),
+          pagination: results,
+        };
+    
+        res.json(formattedResponse);
+    
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Erro ao buscar ordens de serviço" });
+      }
     },
     
-    get: async (req, res) => {
+      get: async (req, res) => {
         try {
             const idParam = req.params.id;
+            const busca = req.query.busca; // Adicione a busca por ID
+            
             let ordem;
     
-            if (isNaN(idParam)) {
-                // Se o parâmetro não for um número, busca pelo ID gerado pelo MongoDB
-                ordem = await OrdemModel.findById(idParam);
+            if (isNaN(idParam) && !busca) {
+                // Se o parâmetro não for um número e não houver busca, retorna um erro
+                res.status(400).json({ msg: "Parâmetro de busca inválido" });
+                return;
+            } 
+    
+            if (isNaN(idParam) && busca) {
+                // Se o parâmetro não for um número, mas houver busca, busca por outros campos
+                ordem = await OrdemModel.findOne({ 
+                    $or: [
+                        { solicitante: { $regex: new RegExp(busca, "i") } }, // Busca por solicitante (case-insensitive)
+                        { setor: { $regex: new RegExp(busca, "i") } }, // Busca por setor (case-insensitive)
+                        // Adicione outros campos que deseja buscar aqui
+                    ]
+                });
             } else {
                 // Se o parâmetro for um número, busca pelo ID personalizado
                 ordem = await OrdemModel.findOne({ ordemId: parseInt(idParam) });
@@ -68,6 +136,7 @@ const ordemController = {
             console.log(error);
         }
     },
+    
     
     delete: async (req, res) => {
         try {
