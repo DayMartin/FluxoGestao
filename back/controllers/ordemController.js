c3ef6db3caace17db18cc81efe0525dee6c30c91
DayMartin <negociosdinah@gmail.com>
2023-11-17 22:02:04 -0300

    Adicionado filtros e contador para setores e status

--- back/controllers/ordemController.js
+++ back/controllers/ordemController.js
@@ -42,30 +42,61 @@ const ordemController = {
         const filter = req.query.filter || '';
         const setor = req.query.setor || '';
         const status = req.query.status || '';
-            const query = OrdemModel.find();
         
+        
+        // Consulta base sem filtros aplicados
+        let query = OrdemModel.find();
+
+        // Aplicar filtro para 'ordemId'
         if (filter) {
           query.where({ ordemId: parseInt(filter) });
         }
 
-        // Adicione a condição para filtrar pelo setor "produção"
+        // Aplicar filtro para o setor "produção"
         if (setor === 'produção') {
           query.where({ setor: 'produção' });
-        }
-
-        if (setor === 'mecânica') {
+        } else if (setor === 'mecânica') { // Aplicar filtro para o setor "mecânica"
           query.where({ setor: 'mecânica' });
         }
-        if (status === 'encerrado') {
-          query.where({ status: 'encerrado' });
+
+        // Aplicar filtro para o status "Aguardando atendimento"
+        if (status === 'Aguardando atendimento') {
+          query.where({ status: 'Aguardando atendimento' });
+        } else if (status === 'Concluido') { // Aplicar filtro para o status "Concluido"
+          query.where({ status: 'Concluido' });
+        } else if (status === 'Em andamento'){
+          query.where({ status: 'Em andamento' });
         }
-    
-        const ordem = await query
-          .skip(skip)
-          .limit(limit);
-    
-        const totalCount = await OrdemModel.countDocuments();
-    
+        else if (status === 'Encerrado'){
+          query.where({ status: 'Encerrado' });
+        }
+
+        // Consulta para contar documentos com base nos filtros aplicados
+        const totalCountQuery = OrdemModel.find();
+
+        // Aplicar os mesmos filtros para a contagem de documentos
+        if (filter) {
+          totalCountQuery.where({ ordemId: parseInt(filter) });
+        }
+
+        if (setor === 'produção') {
+          totalCountQuery.where({ setor: 'produção' });
+        } else if (setor === 'mecânica') {
+          totalCountQuery.where({ setor: 'mecânica' });
+        }
+
+        if (status === 'Aguardando atendimento') {
+          totalCountQuery.where({ status: 'Aguardando atendimento' });
+        } else if (status === 'Concluido') {
+          totalCountQuery.where({ status: 'Concluido' });
+        } else if (status === 'Encerrado'){
+          totalCountQuery.where({status:'Encerrado' })
+        }
+
+        const ordem = await query.skip(skip).limit(limit);
+
+      // Contar documentos com base nos filtros aplicados
+        const totalCount = await totalCountQuery.countDocuments();
         const results = {
           totalOrdem: totalCount,
           pageCount: Math.ceil(totalCount / limit),
