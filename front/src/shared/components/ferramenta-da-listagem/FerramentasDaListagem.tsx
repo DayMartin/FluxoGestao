1432c53c1b3fb9ffa513c692e92bd7b417ed30cc
DayMartin <negociosdinah@gmail.com>
2023-11-17 22:05:26 -0300

    Incluído botão para filtragem de status: Aguardando, Em andamento, Concluído, e contator de quantidade de ordem no status atual, ao final dos botões

--- front/src/shared/components/ferramenta-da-listagem/FerramentasDaListagem.tsx
+++ front/src/shared/components/ferramenta-da-listagem/FerramentasDaListagem.tsx
@@ -1,5 +1,6 @@
 
 import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material"
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { Environment } from "../../environment";
 
@@ -8,16 +9,42 @@ interface IFerramentasDaListagemProps {
     mostrarInputBusca?: boolean;
     aoMudarTextoDeBusca?: (novoTexto: string) => void;
     textoBotaoNovo?: string;
+    textoBotaoConcluido?: string;
+    textoBotaoAguardando?: string;
+    textoBotaoAndamento?: string;
     mostrarBotaoNovo?: boolean;
+    mostrarBotaoConcluido?: boolean;
+    mostrarBotaoAguardando?: boolean;
+    mostrarBotaoAndamento?: boolean;
     aoClicarEmNovo?: () => void;
+    aoClicarEmConcl?: () => void;
+    aoClicarEmAguardando?: () => void;
+    aoClicarEmAndamento?: () => void;
+    quantidadeAguardando?: number;
+    quantidadeConcluido?: number;
+    quantidadeAndamento?: number;
 }
 export const BarraDeFerramentas: React.FC<IFerramentasDaListagemProps> = ({
     textoDabusca = '',
     mostrarInputBusca = false,
     aoMudarTextoDeBusca,
     textoBotaoNovo = 'Novo',
+    textoBotaoConcluido = 'Concluídas',
+    textoBotaoAguardando = 'Aguardando',
+    textoBotaoAndamento = 'Em andamento',
+    mostrarBotaoConcluido = false,
+    mostrarBotaoAguardando = false,
+    mostrarBotaoAndamento = false,
     mostrarBotaoNovo = 'true',
     aoClicarEmNovo ,
+    aoClicarEmConcl,
+    aoClicarEmAguardando,
+    aoClicarEmAndamento,
+    quantidadeAguardando = 0,
+    quantidadeConcluido = 0,
+    quantidadeAndamento = 0,
+    
+    
     
 }) => {
 
@@ -42,6 +69,61 @@ export const BarraDeFerramentas: React.FC<IFerramentasDaListagemProps> = ({
                     onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
                 />
             )}
+
+            <Box flex={1} display={"flex"} justifyContent="end">
+                {mostrarBotaoConcluido && (
+                    <Button
+                    variant='contained'
+                    color="primary"
+                    disableElevation
+                    onClick={aoClicarEmConcl}
+                    style={{ position: 'relative', paddingRight: '40px' }} // Ajuste o paddingRight para acomodar o espaço da quantidade
+                    >
+                    {textoBotaoConcluido}
+                    {/* Exibir a quantidade ao lado do botão */}
+                    {quantidadeConcluido > 0 && (
+                        <span style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>{`(${quantidadeConcluido})`}</span>
+                    )}
+                    </Button>
+                )}
+                </Box>
+
+                <Box flex={1} display={"flex"} justifyContent="start">
+                {mostrarBotaoAguardando && (
+                    <Button
+                    variant='contained'
+                    color="primary"
+                    disableElevation
+                    onClick={aoClicarEmAguardando}
+                    style={{ position: 'relative', paddingRight: '40px' }} // Ajuste o paddingRight para acomodar o espaço da quantidade
+                    >
+                    {textoBotaoAguardando}
+                    {/* Exibir a quantidade ao lado do botão */}
+                    {quantidadeAguardando > 0 && (
+                        <span style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>{`(${quantidadeAguardando})`}</span>
+                    )}
+                    </Button>
+                )}
+                </Box>
+
+                <Box flex={1} display={"flex"} justifyContent="start">
+                {mostrarBotaoAndamento && (
+                    <Button
+                    variant='contained'
+                    color="primary"
+                    disableElevation
+                    onClick={aoClicarEmAndamento}
+                    style={{ position: 'relative', paddingRight: '40px' }} // Ajuste o paddingRight para acomodar o espaço da quantidade
+                    >
+                    {textoBotaoAndamento}
+                    {/* Exibir a quantidade ao lado do botão */}
+                    {quantidadeAndamento > 0 && (
+                        <span style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>{`(${quantidadeAndamento})`}</span>
+                    )}
+                    </Button>
+                )}
+                </Box>
+
 
             <Box flex={1} display={"flex"} justifyContent="end" >
                 {mostrarBotaoNovo && (
