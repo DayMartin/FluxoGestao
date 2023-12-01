bd24134ca57b59eb838a6c9653e1b8ac0866ff58
DayMartin <negociosdinah@gmail.com>
2023-11-30 22:44:39 -0300

    Incluido setor no local storage

--- front/src/shared/contexts/AuthContext.tsx
+++ front/src/shared/contexts/AuthContext.tsx
@@ -8,12 +8,15 @@ interface IAuthContextData {
   login: (email: string, senha: string) => Promise<string | void>;
   role: string | null;
   name: string | null;
+  setor: string | null;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
 const LOCAL_STORAGE_KEY__ACCESS_TOKEN = 'APP_ACCESS_TOKEN';
 const LOCAL_STORAGE_KEY__ACCESS_ROLES = 'APP_ACCESS_ROLES';
 const LOCAL_STORAGE_KEY__ACCESS_USER = 'APP_ACCESS_USER';
+const LOCAL_STORAGE_KEY__ACCESS_SETOR = 'APP_ACCESS_SETOR';
+
 
 
 interface IAuthProviderProps {
@@ -23,11 +26,13 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
   const [token, setToken] = useState<string>();
   const [role, setRole] = useState<string | null>(null);
   const [name, setName] = useState<string | null>(null);
+  const [setor, setSetor] = useState<string | null>(null);
 
   useEffect(() => {
     const Token = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
     const Role = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_ROLES);
     const Name = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_USER);
+    const Setor = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_SETOR);
 
     if (Token) {
       setToken(JSON.parse(Token));
@@ -35,6 +40,8 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
       setToken(JSON.parse(Role)); 
     } else if (Name) {
       setToken(JSON.parse(Name)); 
+    } else if (Setor) {
+      setToken(JSON.parse(Setor)); 
     }
   }, []);
 
@@ -58,10 +65,18 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
       
       if (decodedPayload.name) {
         localStorage.setItem(LOCAL_STORAGE_KEY__ACCESS_USER, JSON.stringify(decodedPayload.name));
-        setName(decodedPayload.name); // Define o valor de name aqui
+        setName(decodedPayload); // Define o valor de name aqui
       } else {
         console.error('decodedPayload.name não está presente ou é inválido');
         // Trate o caso em que decodedPayload.name não está presente ou é inválido
+      }
+
+      if (decodedPayload.setor) {
+        localStorage.setItem(LOCAL_STORAGE_KEY__ACCESS_SETOR, JSON.stringify(decodedPayload.setor));
+        setSetor(decodedPayload.setor[0].name);  // Define o valor de setor aqui
+      } else {
+        console.error('decodedPayload.setor não está presente ou é inválido');
+        // Trate o caso em que decodedPayload.setor não está presente ou é inválido
       }
       
   
@@ -74,17 +89,19 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
     
     localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN);
     localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_ROLES);
-    localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_USER)
+    localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_USER);
+    localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_SETOR)
     setToken(undefined);
     setRole(null);
-    setName(null)
+    setName(null);
+    setSetor(null)
   }, []);
 
   const isAuthenticated = useMemo(() => !!token, [token]);
 
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login: handleLogin, logout: handleLogout, role, name }}>
+    <AuthContext.Provider value={{ isAuthenticated, login: handleLogin, logout: handleLogout, role, name, setor }}>
       {children}
     </AuthContext.Provider>
   );
